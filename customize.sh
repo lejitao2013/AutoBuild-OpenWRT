7d8c4e387e96ed6f1af4407e77e89dbd3780f552
lejitao2013 <61481263+lejitao2013@users.noreply.github.com>
2024-11-28 08:14:19 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,16 +6,16 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.50.1/g' lede/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g'  ./openwrt/lede/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' lede//package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g'  ./openwrt/lede//package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 
 #4 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' lede/package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt''  ./openwrt/lede/package/lean/default-settings/files/zzz-default-settings
 
 #5 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /lejitao build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" lede/package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /lejitao build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g"  ./openwrt/lede/package/lean/default-settings/files/zzz-default-settings
